3c1455805d06168b740298cc8910188d745bb644
Pavan Kumar Kuruva <kpavankumark9783@gmail.com>
2024-08-14 08:26:28 +0530

    Database connected

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const dbConnect = require("./config/dbConnect");
+const cookieParser = require("cookie-parser");
 
 dotenv.config()
+
+// Database Connection
+dbConnect();
+
 
 const hostName = process.env.HOST_NAME || "127.0.0.5"
 const PORT = process.env.PORT || 5000
@@ -9,10 +15,16 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 
+// middleware 
+app.use(express.json());
+app.use(cookieParser())
+
+
 // Demo API
 app.get("/", (req, res) => {
     res.send("Hello, I am API")
 })
+
 
 app.listen(PORT, hostName, () => {
     console.log(`Server running at http://${hostName}:${PORT}`);
